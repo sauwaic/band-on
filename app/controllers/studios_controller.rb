db35edebfc52679cfcf4a692b773788e68cc84d7
thibault de combarieu <tdecomba@gmail.com>
2016-11-30 13:30:05 +0100

    maps for studio's index and show

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,12 +1,18 @@
 class StudiosController < ApplicationController
 
   def index
-    @studios = Studio.all
+
+      @studios = Studio.where.not(latitude: nil, longitude: nil)
+      @hash = Gmaps4rails.build_markers(@studios) do |studio, marker|
+      marker.lat studio.latitude
+      marker.lng studio.longitude
+    end
   end
 
   def show
     @studio = Studio.find(params[:id])
     @available_slots = @studio.slots.where(taken: false)
+    @studio_coordinates = { lat: @studio.latitude, lng: @studio.longitude }
   end
 
 end
