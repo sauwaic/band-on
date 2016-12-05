884c4657ac83265c0462dc7ba258f701e77be285
Sau Wai Chan <sauwaic@gmail.com>
2016-12-05 20:36:05 +0100

    some shows edited

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,12 +7,16 @@ class DashboardsController < ApplicationController
     authorize @group
     @group_admin = @group.admin
     @group_members = @group.users.reject {|user| user == @group.admin}
+    @hash = Gmaps4rails.build_markers(@group.studio) do |studio, marker|
+      marker.lat studio.latitude
+      marker.lng studio.longitude
+    end
   end
 
-  private
+    private
 
-  def set_group
-    @group = Group.find(params[:group_id])
+    def set_group
+      @group = Group.find(params[:group_id])
+    end
+
   end
-
-end
