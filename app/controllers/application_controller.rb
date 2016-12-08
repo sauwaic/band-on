b02c85fd3d3f5264be126fcea107c36d022499f5
Sau Wai Chan <sauwaic@gmail.com>
2016-12-08 15:44:04 +0100

    authentication, details link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,9 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: [:index, :filtered_index], unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
+  before_filter :store_current_location, :unless => :devise_controller?
+
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :description, :experience, :genre])
@@ -19,6 +22,10 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)|(^messages$)/
+  end
+
+  def store_current_location
+    store_location_for(:user, request.url)
   end
 
 end
